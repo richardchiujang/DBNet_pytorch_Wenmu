518570c670eff340100f3266e5af7e546527a456
richard chiu <rchiu3210@gmail.com>
2024-06-18 23:04:08 +0800

    v5.3

--- singlel_gpu_train.bat
+++ singlel_gpu_train.bat
@@ -1,4 +1,9 @@
+REM activate conda env trainocr 
+call C:\anaconda3\Scripts\activate.bat C:\anaconda3
+call conda activate trainocr
+cd C:\develop\DBNet_pytorch_Wenmu
+
 python tools\train.py --config_file "config/icdar2015_resnet18_FPN_DBhead_polyLR.yaml"
-python tools\train.py --config_file "config/icdar2015_resnet18_FPN_DBhead_polyLR.yaml"
-python tools\train.py --config_file "config/icdar2015_resnet18_FPN_DBhead_polyLR.yaml"
-python tools\train.py --config_file "config/icdar2015_resnet18_FPN_DBhead_polyLR.yaml"
+REM python tools\train.py --config_file "config/icdar2015_resnet18_FPN_DBhead_polyLR.yaml"
+REM python tools\train.py --config_file "config/icdar2015_resnet18_FPN_DBhead_polyLR.yaml"
+REM python tools\train.py --config_file "config/icdar2015_resnet18_FPN_DBhead_polyLR.yaml"
